24f5f5861d6fab2f01648228cb613bdc0bbaa20a
Gabriel Matuk <gabrielmatuk14@gmail.com>
2022-01-30 22:28:22 -0300

    change to middleware

--- controllers/aluno.controller.js
+++ controllers/aluno.controller.js
@@ -1,22 +1,22 @@
-import UsuarioService from "../service/aluno.service.js"
+import UsuarioService from "../service/aluno.service.js";
 //Utilizando arrow functions por conta do ES6
 const createUsuario = async (req, res, next) => {
   try {
     //Body da requisição
-    let usuario = (req.body);
-    //Determinei que os campos nome email e curso são obrigatorios, caso não tenha vou ter um 400 e mensagem de erro, além de mostrar no console
-    if (!usuario.nome || !usuario.email || !usuario.curso) {
-      res.status(400)
-      res.send('Nome, Email e Curso do aluno são obrigatórios!')
-      throw new Error("Nome, Email e Curso do aluno são obrigatórios!")
+    let usuario = req.body;
+    //Determinei que os campos nome email e endereço são obrigatorios, caso não tenha vou ter um 400 e mensagem de erro, além de mostrar no console
+    if (!usuario.nome || !usuario.email || !usuario.endereco) {
+      res.status(400);
+      res.send("Nome, Email e Endereço do aluno são obrigatórios!");
+      throw new Error("Nome, Email e Endereço do aluno são obrigatórios!");
     }
-    usuario = await UsuarioService.createUsuario(usuario)
+    usuario = await UsuarioService.createUsuario(usuario);
 
-    res.send(usuario)
+    res.send(usuario);
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 //Tratativa de chamado para ser feito no endpoint. Não passo nenhum parametro
 const getAlunos = async (_, res, next) => {
   try {
@@ -24,7 +24,7 @@ const getAlunos = async (_, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 //Tratativa de chamado para ser feito no endpoint. Passo parametro de ID
 const getAluno = async (req, res, next) => {
   try {
@@ -32,36 +32,36 @@ const getAluno = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 //Tratativa de chamado para ser feito no endpoint. Passo parametro de ID
 const deleteAluno = async (req, res, next) => {
   try {
     await UsuarioService.deleteAluno(req.params.id);
     res.end();
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 //Tratativa de chamado para ser feito no endpoint. Não passo nenhum parametro
 const updateAluno = async (req, res, next) => {
   try {
     let usuario = req.body;
 
-    if (!usuario.nome || !usuario.email || !usuario.curso) {
-      res.status(400)
-      res.send('Nome, Email e Curso do aluno são obrigatórios!')
-      throw new Error("Nome, Email e Curso do aluno são obrigatórios!")
+    if (!usuario.nome || !usuario.email || !usuario.endereco) {
+      res.status(400);
+      res.send("Nome, Email e Endereço do aluno são obrigatórios!");
+      throw new Error("Nome, Email e Endereço do aluno são obrigatórios!");
     }
-    res.send(await UsuarioService.updateAluno(usuario))
+    res.send(await UsuarioService.updateAluno(usuario));
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 export default {
   createUsuario,
   getAlunos,
   getAluno,
   deleteAluno,
-  updateAluno
-}
+  updateAluno,
+};
--- repositories/aluno.repository.js
+++ repositories/aluno.repository.js
@@ -1,5 +1,5 @@
 import { promises as fs } from "fs";
-const { readFile, writeFile } = fs
+const { readFile, writeFile } = fs;
 
 //Função para inserir o usuário no meu array de ALUNOS e fazer somatoria no meu nextId
 const insertUsuario = async (usuario) => {
@@ -9,57 +9,61 @@ const insertUsuario = async (usuario) => {
     id: data.nextId++,
     nome: usuario.nome,
     email: usuario.email,
-    curso: usuario.curso
+    endereco: usuario.endereco,
   };
   data.alunos.push(usuario);
 
   await writeFile(global.filename, JSON.stringify(data, null, 2));
 
-  return usuario
-}
+  return usuario;
+};
 //Função para listar TODOS os alunos
 const getAlunos = async () => {
   const data = JSON.parse(await readFile(global.filename));
   return data.alunos;
-}
+};
 //Função para listar aluno especifico
 const getAluno = async (id) => {
   const alunos = await getAlunos();
-  const usuario = alunos.find(usuario => usuario.id === parseInt(id));
+  const usuario = alunos.find((usuario) => usuario.id === parseInt(id));
   if (usuario) {
-    return usuario
+    return usuario;
   }
-  throw new Error("Aluno não encontrado")
-}
+  throw new Error("Aluno não encontrado");
+};
 //Função para deletar um aluno
 const deleteAluno = async (id) => {
   const data = JSON.parse(await readFile(global.filename));
-  data.alunos = data.alunos.filter(usuario => usuario.id === parseInt(id));
+  data.alunos = data.alunos.filter((usuario) => usuario.id === parseInt(id));
+  let index = data.alunos;
 
+  if (index === -1) {
+    throw new Error("Erro! Usuário não encontrado!");
+  }
   await writeFile(global.filename, JSON.stringify(data, null, 2));
-  console.log(`USUÁRIO DO ID: ${id} FOI CANCELADO COM SUCESSO`)
-}
+  console.log(`USUÁRIO DO ID: ${id} FOI CANCELADO COM SUCESSO`);
+};
 //Função para ATUALIZAR um aluno
 const updateAluno = async (usuario) => {
   const data = JSON.parse(await readFile(global.filename));
-  const index = data.alunos.findIndex(a => a.id === usuario.id);
+  const index = data.alunos.findIndex((a) => a.id === usuario.id);
 
   if (index === -1) {
-    throw new Error("ID não encontrado")
+    throw new Error("Erro! Usuário não encontrado!");
   }
   data.alunos[index].nome = usuario.nome;
   data.alunos[index].email = usuario.email;
-  data.alunos[index].curso = usuario.curso;
+  data.alunos[index].endereco = usuario.endereco;
 
   await writeFile(global.filename, JSON.stringify(data, null, 2));
 
-  return data.alunos[index]
-}
+  return data.alunos[index];
+};
 
 export default {
   insertUsuario,
   getAlunos,
   getAluno,
   deleteAluno,
-  updateAluno
-}
+  updateAluno,
+};
